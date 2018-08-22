db5873e5ef817395c13e34afd04f3fa0adf8c9e7
mikotova <katemikotova@gmail.com>
2018-08-22 18:45:31 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,39 +6,58 @@ using System.Threading.Tasks;
 
 namespace QATestLabGame
 {
-    class HumanMage
+    class Fighter
     {
+        public int hp;
+        public int atack;
+        public string name;
+        public int fight(int enemyHp)
+        {                                               //метод атаки по сопернику
+            return enemyHp - atack;
+        }
+    }
+    class HumanMage:Fighter
+    {
+
         public string name;
         public int hp=100;
         public int atack = 10;
         public bool upgraded = false;
-        public int fight(int enemyHp){              //метод атаки по сопернику
-            return enemyHp - atack;
+
+        public HumanMage(string name)
+        {
+            this.name = name;
         }
         public double upgrade(int friendAtack)             //метод улучшения союзника
         {
             return friendAtack*1.5;
         }
     }
-    class HumanFighter {
+    class HumanFighter: Fighter {
         public string name;
-        public int hp = 100;
+        private new int hp = 100;
         public int atack = 15;
-        public bool upgraded = false;
-        public int fight(int enemyHp)
-        {                                               //метод атаки по сопернику
-            return enemyHp - atack;
+        public bool upgraded =false;
+
+        public int Hp { get => hp; set => hp = value; }
+
+        public HumanFighter(string name)
+        {
+            this.name = name;
         }
+
+        
     }
-    class HumanArcher {
+    class HumanArcher : Fighter{
         public string name;
         public int hp = 100;
         public bool upgraded = false;
-        public int miliAtack = 3;
+        public int atack = 3;
         public int rangedAtack = 7;
-        public int miliFight(int enemyHp)
-        {                                                    //метод ближней атаки по сопернику
-            return enemyHp - miliAtack;
+
+        public HumanArcher(string name)
+        {
+            this.name = name;
         }
         public int rangedFight(int enemyHp)
         {                                                    //метод дальней атаки по сопернику
@@ -55,11 +74,17 @@ namespace QATestLabGame
     }
     class OrcFighter { }
     class OrcArcher { }
-    class DeadMage {
+    class DeadMage : Fighter {
         public string name;
         public int hp = 100;
         public int atack = 5;
         public bool upgraded = false;
+
+        public DeadMage(string name)
+        {
+            this.name = name;
+        }
+
         public int fight(int enemyHp)
         {
             return enemyHp - atack;
@@ -69,12 +94,18 @@ namespace QATestLabGame
             return enemyAtack * 0.5;
         }
     }
-    class DeadArcher {
+    class DeadArcher: Fighter {
         public string name;
         public int hp = 100;
         public bool upgraded = false;
         public int miliAtack = 2;
         public int rangedAtack = 4;
+
+        public DeadArcher(string name)
+        {
+            this.name = name;
+        }
+
         public int miliFight(int enemyHp)
         {                                                    //метод ближней атаки по сопернику
             return enemyHp - miliAtack;
@@ -84,14 +115,15 @@ namespace QATestLabGame
             return enemyHp - rangedAtack;
         }
     }
-    class DeadFighter {
+    class DeadFighter: Fighter{
         public string name;
         public int hp = 100;
         public int atack = 18;
         public bool upgraded = false;
-        public int fight(int enemyHp)
-        {                                               //метод атаки по сопернику
-            return enemyHp - atack;
+
+        public DeadFighter(string name)
+        {
+            this.name = name;
         }
     }
 
@@ -133,18 +165,30 @@ namespace QATestLabGame
             OrcArcher orcarcher1 = null;
             OrcArcher orcarcher2 = null;
             OrcArcher orcarcher3 = null;
+            
             Random rand = new Random();
-            if (rand.Next(0, 2) == 0)                                   //Создание силы света
+            List<Fighter> lightList = new List<Fighter>();
+            List<Fighter> darkList = new List<Fighter>();
+            //   if (rand.Next(0, 2) == 0)                                   //Создание силы света
+            if (true)
             {
                 Console.WriteLine("Первый отряд состоит из людей.");
-                mage1 = new HumanMage();
-                fighter1 = new HumanFighter();
-                fighter2 = new HumanFighter();
-                fighter3 = new HumanFighter();
-                fighter4 = new HumanFighter();
-                archer1 = new HumanArcher();
-                archer2 = new HumanArcher();
-                archer3 = new HumanArcher();
+                mage1 = new HumanMage("Человек маг");
+                fighter1 = new HumanFighter("Первый человек воин");
+                fighter2 = new HumanFighter("Второй человек воин");
+                fighter3 = new HumanFighter("Третий человек воин");
+                fighter4 = new HumanFighter("Четвертый человек воин");
+                archer1 = new HumanArcher("Первый человек лучник");
+                archer2 = new HumanArcher("Второй человек лучник");
+                archer3 = new HumanArcher("Третий человек лучник");
+                lightList.Add(mage1);                                                               //todo доработать
+                lightList.Add(fighter1);
+                lightList.Add(fighter2);
+                lightList.Add(fighter3);
+                lightList.Add(fighter4);
+                lightList.Add(archer1);
+                lightList.Add(archer2);
+                lightList.Add(archer3);
             }
             else
             {
@@ -158,7 +202,8 @@ namespace QATestLabGame
                 elfarcher2 = new ElfArcher();
                 elfarcher3 = new ElfArcher();
             }
-                if (rand.Next(0, 2) == 0)                               //создание силы тьмы
+            //           if (rand.Next(0, 2) == 0)                               //создание силы тьмы
+            if(false)
             {
                 Console.WriteLine("Второй отряд состоит из орков.");
                 orcmage1 = new OrcMage();
@@ -172,31 +217,62 @@ namespace QATestLabGame
             }
             else
             {
-                Console.WriteLine("Второй отряд состоит из некромантов.");
-                dmage1 = new DeadMage();
-                dfighter1 = new DeadFighter();
-                dfighter2 = new DeadFighter();
-                dfighter3 = new DeadFighter();
-                dfighter4 = new DeadFighter();
-                darcher1 = new DeadArcher();
-                darcher2 = new DeadArcher();
-                darcher3 = new DeadArcher();
+                Console.WriteLine("Второй отряд состоит из нежити.");
+                dmage1 = new DeadMage("Некромант");
+                dfighter1 = new DeadFighter("Первый зомби");
+                dfighter2 = new DeadFighter("Второй зомби");
+                dfighter3 = new DeadFighter("Третий зомби");
+                dfighter4 = new DeadFighter("Четвертый зомби");
+                darcher1 = new DeadArcher("Первый охотник");
+                darcher2 = new DeadArcher("Второй охотник");
+                darcher3 = new DeadArcher("Третий охотник");
+                darkList.Add(dmage1);                                                               //todo доработать
+                darkList.Add(dfighter1);
+                darkList.Add(dfighter2);
+                darkList.Add(dfighter3);
+                darkList.Add(dfighter4);
+                darkList.Add(darcher1);
+                darkList.Add(darcher2);
+                darkList.Add(darcher3);
             }
-            //todo: геймплей
-            //while (mage1.hp > 0 && dmage1.hp > 0)
-            //{
-            //    mage1.hp = dmage1.fight(mage1.hp);
-            //    Console.WriteLine($"Некромант нанес атаку магу. ХП мага: {mage1.hp}");
-            //    dmage1.hp = mage1.fight(dmage1.hp);
-            //    Console.WriteLine($"Маг нанес атаку некроманту. ХП некроманта: {dmage1.hp}");
-            //}
 
-            Console.ReadKey();
+            string player = lightList[1].GetType().Name;
+            foreach (var list in lightList) {  
+                switch (list.GetType().Name)
+                {
+                    case "HumanFighter":
+                        {
+                            int whom = rand.Next(0, 8);
+                            list.fight(darkList[whom].hp);
+                            Console.WriteLine(list.name);
+                            Console.WriteLine(list.name, "атаковал", darkList[whom].name);
+                            break;
+                        }
+
+                    case "HumanMage":
+                        break;
+                    case "HumanArcher":
+                        break;
+                }
+            }
+
+                //todo: геймплей
+                //while (mage1.hp > 0 && dmage1.hp > 0)
+                //{
+                //    mage1.hp = dmage1.fight(mage1.hp);
+                //    Console.WriteLine($"Некромант нанес атаку магу. ХП мага: {mage1.hp}");
+                //    dmage1.hp = mage1.fight(dmage1.hp);
+                //    Console.WriteLine($"Маг нанес атаку некроманту. ХП некроманта: {dmage1.hp}");
+                //}
+
+                Console.ReadKey();
         }
-        static int StartGame()
+        static int StartGame(List<object> myList)
         {
+            
             return 0;
         }
+        
        
     }
 }
